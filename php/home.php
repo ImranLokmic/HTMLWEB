41505f5a12e2a28d7dcc6e4b18d43f116fd205ef
ImranLokmic <imranlokmic99@gmail.com>
2020-03-30 12:55:56 +0200

    PHP attempt #1

---
+++ php/home.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  $conn = new mysql($servername, $username, $password);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+  echo "string";
+ ?>
