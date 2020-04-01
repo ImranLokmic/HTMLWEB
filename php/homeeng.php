7ae1373f6e5dfa37cdeb426400dd1320a08b3a9d
ImranLokmic <imranlokmic99@gmail.com>
2020-04-01 16:23:39 +0200

    php fix

--- php/homeeng.php
+++ php/homeeng.php
@@ -11,16 +11,15 @@
     die("Connection failed: " . $conn->connect_error);
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $ime=$_POST['ime'] ;
-  $cvv=$_POST['cvv'];
-  $card=$_POST['card'];
-  $mjesec=$_POST['mjesec'];
-  $godina=$_POST['godina'];
+  $_SESSION['ime']=$_POST['ime'] ;
+  $_SESSION['cvv']=$_POST['cvv'];
+  $_SESSION['card']=$_POST['card'];
+  $_SESSION['mjesec']=$_POST['mjesec'];
+  $_SESSION['godina']=$_POST['godina'];
 }
 $query = mysqli_query($conn,"SELECT * FROM card");
     while($row = $query->fetch_assoc()) {
-        if ($ime == $row["Name"] && $cvv == $row["CVV"] && $card == $row["Card_Number"] && $mjesec == $row["Month"] && $godina == $row["Year"]) {
-          //echo $row["Name"];
+        if ($_SESSION['ime'] == $row["Name"] && $_SESSION['cvv'] == $row["CVV"] && $_SESSION['card'] == $row["Card_Number"] && $_SESSION['mjesec'] == $row["Month"] && $_SESSION['godina'] == $row["Year"]) {
           header("Location: http://localhost/htmlweb2/pages/pineng.html");
           die();
         }
